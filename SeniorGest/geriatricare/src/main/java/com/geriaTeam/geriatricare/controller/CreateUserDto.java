31b2c928edb10a8b8253e4ea6d929ce531d1bdad
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-03-06 18:58:33 -0300

    .

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/controller/CreateUserDto.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/controller/CreateUserDto.java
@@ -1,4 +1,4 @@
-package tech.buildrun.agregadorinvestimentos.controller;
+package com.geriaTeam.geriatricare.controller;
 
 public record CreateUserDto(String username, String email, String password) {
 }
--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/controller/UpdateUserDto.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/controller/UpdateUserDto.java
@@ -1,4 +1,4 @@
-package tech.buildrun.agregadorinvestimentos.controller;
+package com.geriaTeam.geriatricare.controller;
 
 public record UpdateUserDto(String username, String password) {
 }
