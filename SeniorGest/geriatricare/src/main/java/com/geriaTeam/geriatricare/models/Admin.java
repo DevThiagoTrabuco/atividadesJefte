4c32298de87a9e6916f028fd18260dbbf50107cb
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-03-09 21:36:14 -0300

    updates

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/Admin.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/Admin.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import java.util.UUID;
 
 @Entity
+@Setter
+@Getter
 @Table(name = "Administrador")
 public class Admin {
     @Id
@@ -18,37 +20,4 @@ public class Admin {
 
     @Column(name = "senhaAdministrador")
     private String password;
-
-    public Admin(UUID id, String user, String password) {
-        this.id = id;
-        this.user = user;
-        this.password = password;
-    }
-    public Admin() {
-
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/Familiar.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/Familiar.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 @Table(name = "Familiar")
 public class Familiar {
     @Id
@@ -26,56 +28,5 @@ public class Familiar {
     @ManyToMany(mappedBy = "familiares")
     private List<Paciente> pacients;
 
-    public Familiar(UUID id, String name, String phone, String email, List<Paciente> pacients) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.pacients = pacients;
 
-    }
-
-    public Familiar() {
-
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Paciente> getPacients() {
-        return pacients;
-    }
-
-    public void setPacients(List<Paciente> pacients) {
-        this.pacients = pacients;
-    }
 }
