d34700a42a8c7e3c9b0409c193aef5d697205c73
WdKitsune <187020619+Matheca123@users.noreply.github.com>
2025-03-17 20:05:10 -0300

    update_sinaisVitais

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/Interfaces/SinaisVitaisRepository.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/Interfaces/SinaisVitaisRepository.java
@@ -1,5 +1,13 @@
 package com.geriaTeam.geriatricare.Interfaces;
 
-public interface SinaisVitaisRepository {
+import java.util.List;
 
+import com.geriaTeam.geriatricare.models.domain.SinaisVitais;
+
+public interface SinaisVitaisRepository {
+    public SinaisVitais buscarPorCodigo(int codigo);
+    public List<SinaisVitais> buscar();
+    public void adicionar(SinaisVitais sinaisvitais);
+    public void remover(int codigo);
+    public void atualizar(int codigo, SinaisVitais sinaisvitais);
 }
