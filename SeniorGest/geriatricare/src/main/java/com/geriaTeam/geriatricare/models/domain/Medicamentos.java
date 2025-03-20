b5d960d75a12dd26cb3a0bca78596f1d9ea8ed4f
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-03-20 10:25:45 -0300

    Update Medicamentos.java

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Medicamentos.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Medicamentos.java
@@ -1,22 +1,15 @@
 package com.geriaTeam.geriatricare.models.domain;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "medicamentos")
 public class Medicamentos {
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    @Column(name = "medicamentos_id")
-    private int medicamentosId;
+    
+    private int id;
 
-    @Column(name = "nome")
     private String nome;
 
-    @Column(name = "qtdMedicamentos")
     private int qtdMedicamentos;
 }
