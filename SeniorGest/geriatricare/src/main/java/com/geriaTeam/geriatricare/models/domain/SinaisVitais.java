e47fead07989231b3a03224082d94532d3f274e5
Matheca123 <mateuscorreia14@gmail.com>
2025-03-19 22:03:15 -0300

    Update SinaisVitais.java

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/SinaisVitais.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/SinaisVitais.java
@@ -6,13 +6,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SinaisVitais {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "batimentos")
     private int batimentos;
 
+    @Column(name = "oximetria")
     private int oximetria;
 
+    @Column(name = "temperatura")
     private int temperatura;
 
+    @Column(name = "idPaciente")
     private int idPaciente;
 }
