d5238876c186bf4df8a74d930979592e4b7a6413
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2025-03-21 17:32:01 -0300

    Update_Funcionario_Paciente

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Funcionario.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Funcionario.java
@@ -6,11 +6,26 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "funcionarios")
 public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "funcionarios_id")
     private int id;
+
+    @Column(name = "funcionarios_nome")
     private String nome;
+
+    @Column(name = "funcionarios_nascimento")
     private LocalDate nascimento;
+
+    @Column(name = "funcionarios_telefone")
     private int telefone;
+
+    @Column(name = "funcionarios_email")
     private String email;
+
+    @Column(name = "funcionarios_funcao")
     private int funcao;
 }
--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Paciente.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Paciente.java
@@ -7,11 +7,26 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "pacientes")
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pacientes_id")
     private int id;
+
+    @Column(name = "pacientes_nome")
     private String nome;
+
+    @Column(name = "pacientes_nascimento")
     private LocalDate nascimento;
+
+    @Column(name = "pacientes_entrada")
     private LocalDateTime entrada;
+
+    @Column(name = "pacientes_condMental")
     private String condMental;
+
+    @Column(name = "pacientes_condFisica")
     private String condFisica;
 }
