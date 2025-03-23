6909ff710e64374e27510fc716c6c6cbc5c29e78
DevThiagoTrabuco <thiago.m.lima@ba.estudante.senai.br>
2025-03-23 20:17:07 -0300

    update_indicador

---
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/controller/IndicadorController.java
@@ -0,0 +1,53 @@
+package com.geriaTeam.geriatricare.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.geriaTeam.geriatricare.facade.IndicadorFacade;
+import com.geriaTeam.geriatricare.models.domain.Indicador;
+
+@RequestMapping("/indicador/")
+@RestController
+public class IndicadorController {
+    private final IndicadorFacade indicadorFacade;
+
+    @Autowired
+    public IndicadorController(IndicadorFacade indicadorFacade) {
+        this.indicadorFacade = indicadorFacade;
+
+    }
+
+    @GetMapping("")
+    public List<Indicador> buscar(){
+        return indicadorFacade.buscar();
+    }
+
+    @GetMapping("/{codigo}")
+    public Indicador buscarPorCodigo(@PathVariable int codigo){
+        return indicadorFacade.buscarPorCodigo(codigo);
+    }
+
+    @PostMapping("")
+    public void adicionar(@RequestBody Indicador indicador){
+        indicadorFacade.adicionar(indicador);
+    }
+
+    @PutMapping("/{codigo}")
+    public void atualizar(@PathVariable int codigo, @RequestBody Indicador indicador){
+        indicadorFacade.atualizar(codigo, indicador);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable int codigo){
+        indicadorFacade.remover(codigo);
+    }
+}
--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Indicador.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/models/domain/Indicador.java
@@ -1,18 +1,26 @@
 package com.geriaTeam.geriatricare.models.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
-public class SinaisVitais {
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "indicadores")
+public class Indicador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "indicadores_id")
     private int id;
 
+    @Column(name = "indicadores_batimentos")
     private int batimentos;
 
+    @Column(name = "indicadores_oximetria")
     private int oximetria;
 
+    @Column(name = "indicadores_temperatura")
     private int temperatura;
-
-    private int idPaciente;
 }
