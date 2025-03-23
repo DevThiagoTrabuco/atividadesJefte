b785d499b196b319822336c7b42f8d3fe631ff5c
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-03-23 18:50:30 -0300

    update medicamento

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/repositories/jpa/FuncionarioJPA.java
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/repositories/jpa/FuncionarioJPA.java
@@ -3,6 +3,6 @@ package com.geriaTeam.geriatricare.repositories.jpa;
 import com.geriaTeam.geriatricare.models.domain.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FuncionarioJPA extends JpaRepository<Funcionario, Integer {
+public interface FuncionarioJPA extends JpaRepository<Funcionario, Integer >{
 
 }
--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/repositories/jpa/MedicamentosJPA.java
+++
@@ -1,7 +0,0 @@
-package com.geriaTeam.geriatricare.repositories.jpa;
-
-import com.geriaTeam.geriatricare.models.domain.Medicamentos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicamentosJPA extends JpaRepository<Medicamentos, Integer> {
-}
