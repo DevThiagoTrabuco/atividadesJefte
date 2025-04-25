8715faa65b82ab29745e7757773f75f9b68b4174
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-04-25 13:54:15 -0300

    feat: add entites intefaces and enums

---
+++ SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/entities/enums/CondicaoFisicaEnums.java
@@ -0,0 +1,14 @@
+package com.geriaTeam.geriatricare.entities.enums;
+
+public enum CondicaoFisicaEnums {
+    MOBILIDADE_REDUZIDA,
+    DEFICIENCIA_VISUAL,
+    DEFICIENCIA_AUDITIVA,
+    DOENCA_CARDIACA,
+    DIABETES,
+    HIPERTENSAO,
+    OBESIDADE,
+    DOENCA_RESPIRATORIA,
+    DOENCA_RENAL,
+    OUTROS
+}
