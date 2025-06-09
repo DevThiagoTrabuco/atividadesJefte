4e65dc79d1193c3ad181c8d986f90f8925e25664
DevThiagoTrabuco <thiago.m.lima@ba.estudante.senai.br>
2025-06-09 20:31:53 -0300

    Revert "revert"
    This reverts commit 7f1ee7a5059945d6cedded93b7953ecfe77e4890.

--- SeniorGest/geriatricare/src/main/java/com/geriaTeam/geriatricare/repositories/jpa/PacienteJPA.java
+++
@@ -1,8 +0,0 @@
-package com.geriaTeam.geriatricare.repositories.jpa;
-
-import com.geriaTeam.geriatricare.models.PacienteModels;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteJPA extends JpaRepository<PacienteModels, Integer>{
-
-}
