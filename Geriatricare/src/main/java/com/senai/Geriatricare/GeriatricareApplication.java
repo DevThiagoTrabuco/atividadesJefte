2642e27a8b78ceed1230e69b818864b2ae729fc5
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-06-11 13:45:03 -0300

    Update GeriatricareApplication.java

--- Geriatricare/src/main/java/com/senai/Geriatricare/GeriatricareApplication.java
+++ Geriatricare/src/main/java/com/senai/Geriatricare/GeriatricareApplication.java
@@ -9,6 +9,6 @@ public class GeriatricareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeriatricareApplication.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
+
 	}
 }
